ca56b17e53d9bdfef6c962371d7c5e27b3142505
Pitmairen <progrper@gmail.com>
2017-02-09 13:34:05 +0100

    Added simple test gui implementation for example layout

--- app/src/main/java/no/ntnu/stud/avikeyb/MainActivity.java
+++ app/src/main/java/no/ntnu/stud/avikeyb/MainActivity.java
@@ -1,13 +1,65 @@
 package no.ntnu.stud.avikeyb;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import no.ntnu.stud.avikeyb.backend.InputType;
+import no.ntnu.stud.avikeyb.backend.OutputDevice;
+import no.ntnu.stud.avikeyb.backend.core.CoreKeyboard;
+import no.ntnu.stud.avikeyb.backend.layouts.SimpleExampleLayout;
+import no.ntnu.stud.avikeyb.gui.SimpleExampleLayoutGUI;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CoreKeyboard keyboard;
+    private SimpleExampleLayout layout;
+    private SimpleExampleLayoutGUI layoutGUI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        keyboard = new CoreKeyboard();
+        keyboard.addOutputDevice(new ToastOutput());
+
+        layout = new SimpleExampleLayout(keyboard);
+        layoutGUI = new SimpleExampleLayoutGUI(this, keyboard, layout);
+
+
+        ViewGroup layoutWrapper = (ViewGroup) findViewById(R.id.layoutWrapper);
+        layoutWrapper.addView(layoutGUI.buildGUI());
+
+
+        // Use buttons to register the inputs
+        ((Button) findViewById(R.id.buttonRight)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Turn the input on and off for each click
+                layout.setInputState(InputType.INPUT2, true);
+                layout.setInputState(InputType.INPUT2, false);
+            }
+        });
+        ((Button) findViewById(R.id.buttonSelect)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                layout.setInputState(InputType.INPUT1, true);
+                layout.setInputState(InputType.INPUT1, false);
+            }
+        });
+
+    }
+
+
+    // Shows the keyboard output in a toast message
+    private class ToastOutput implements OutputDevice {
+        @Override
+        public void sendOutput(String output) {
+            Toast.makeText(MainActivity.this, output, Toast.LENGTH_SHORT).show();
+        }
     }
 }
---
+++ app/src/main/java/no/ntnu/stud/avikeyb/gui/SimpleExampleLayoutGUI.java
@@ -0,0 +1,110 @@
+package no.ntnu.stud.avikeyb.gui;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import no.ntnu.stud.avikeyb.R;
+import no.ntnu.stud.avikeyb.backend.Keyboard;
+import no.ntnu.stud.avikeyb.backend.Layout;
+import no.ntnu.stud.avikeyb.backend.layouts.SimpleExampleLayout;
+
+/**
+ * Created by pitmairen on 09/02/2017.
+ */
+
+public class SimpleExampleLayoutGUI {
+
+    private Activity activity;
+
+    // Store a reference to all the symbol for easy access when updating the gui
+    private List<View> symbolViews;
+    private SimpleExampleLayout layout;
+    private Keyboard keyboard;
+
+    public SimpleExampleLayoutGUI(Activity activity, Keyboard keyboard, SimpleExampleLayout layout) {
+
+        this.layout = layout;
+        this.activity = activity;
+        this.keyboard = keyboard;
+
+        symbolViews = new ArrayList<>();
+
+        // Update the gui when the layout state changes
+        layout.addLayoutListener(new Layout.LayoutListener() {
+            @Override
+            public void onLayoutChanged() {
+                updateGUI();
+            }
+        });
+
+
+    }
+
+    // Build the gui programmatically
+    public ViewGroup buildGUI() {
+
+        ScrollView rootWrapper = new ScrollView(activity); // Make the keyboard symbols scrollable
+
+        LinearLayout root = new LinearLayout(activity); // The root layout containing all the symbols
+        root.setOrientation(LinearLayout.VERTICAL);
+
+        rootWrapper.addView(root);
+
+        for (int i = 0; i < layout.getSymbolCount(); ) {
+
+            LinearLayout row = new LinearLayout(activity);
+            row.setOrientation(LinearLayout.HORIZONTAL);
+
+            // Create rows with 10 symbols on each row
+
+            for (int j = 0; i < layout.getSymbolCount() && j < 10; i++, j++) {
+
+                TextView view = new TextView(activity);
+
+                LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, 100, 100);
+                lp.setMargins(5, 5, 5, 5);
+                view.setLayoutParams(lp);
+                view.setGravity(Gravity.CENTER);
+                view.setText(layout.getSumbolAt(i).getContent());
+
+                symbolViews.add(view);
+                row.addView(view);
+            }
+            root.addView(row);
+        }
+
+        updateGUI();
+        return rootWrapper;
+    }
+
+
+    private void updateGUI() {
+
+        // Update the current buffer view
+        ((TextView) activity.findViewById(R.id.currentBuffer)).setText(keyboard.getCurrentBuffer());
+
+        // Highlight the selected symbol
+        int current = layout.getCurrentPosition();
+        for (int i = 0; i < symbolViews.size(); i++) {
+
+            if (current == i) {
+                symbolViews.get(i).setBackgroundColor(Color.GREEN);
+            } else {
+                symbolViews.get(i).setBackgroundColor(Color.GRAY);
+            }
+
+        }
+
+
+    }
+
+}
