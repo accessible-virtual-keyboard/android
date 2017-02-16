7b71aac63ad2a1cfc31b7a555270d5e1d0768c46
Pitmairen <progrper@gmail.com>
2017-02-16 11:30:21 +0100

    Tweaked styling of binsearch layout

--- app/src/main/java/no/ntnu/stud/avikeyb/gui/BinarySearchLayoutGUI.java
+++ app/src/main/java/no/ntnu/stud/avikeyb/gui/BinarySearchLayoutGUI.java
@@ -2,6 +2,7 @@ package no.ntnu.stud.avikeyb.gui;
 
 import android.app.Activity;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -41,6 +42,7 @@ public class BinarySearchLayoutGUI extends LayoutGUI {
             if (loader.hasSymbol(symbol)) {
                 TextView view = (TextView) loader.getViewForSymbol(symbol);
                 view.setText(symbol.getContent());
+                view.setTypeface(null, Typeface.BOLD);
                 symbolViewMap.put(symbol, view);
             }
         }
@@ -54,12 +56,19 @@ public class BinarySearchLayoutGUI extends LayoutGUI {
 
             if (layout.symbolIsActive(it.getKey())) {
 
+                if(it.getValue() instanceof TextView){
+                    ((TextView)it.getValue()).setTextColor(Color.parseColor("#333333"));
+                }
+
                 if (layout.symbolIsActiveLeft(it.getKey()))
-                    it.getValue().setBackgroundColor(Color.GREEN);
+                    it.getValue().setBackgroundColor(Color.parseColor("#5AD035"));
                 else
-                    it.getValue().setBackgroundColor(Color.RED);
+                    it.getValue().setBackgroundColor(Color.parseColor("#ff4f68"));
             } else {
-                it.getValue().setBackgroundColor(Color.GRAY);
+                it.getValue().setBackgroundColor(Color.parseColor("#eeeeee"));
+                if(it.getValue() instanceof TextView){
+                    ((TextView)it.getValue()).setTextColor(Color.parseColor("#aaaaaa"));
+                }
             }
         }
     }
