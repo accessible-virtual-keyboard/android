845a0090c2dd7b111ce021090f5231a3a257c5ac
Pitmairen <progrper@gmail.com>
2017-02-22 09:47:54 +0100

    Fixed bin search layout test

--- app/src/test/java/no/ntnu/stud/avikeyb/backend/layouts/BinarySearchLayoutTest.java
+++ app/src/test/java/no/ntnu/stud/avikeyb/backend/layouts/BinarySearchLayoutTest.java
@@ -2,8 +2,14 @@ package no.ntnu.stud.avikeyb.backend.layouts;
 
 import org.junit.Test;
 
+import java.util.Collections;
+import java.util.List;
+
+import no.ntnu.stud.avikeyb.backend.Dictionary;
 import no.ntnu.stud.avikeyb.backend.InputType;
 import no.ntnu.stud.avikeyb.backend.Layout;
+import no.ntnu.stud.avikeyb.backend.Suggestions;
+import no.ntnu.stud.avikeyb.backend.core.SingleThreadSuggestions;
 
 /**
  * Created by pitmairen on 08/02/2017.
@@ -12,7 +18,17 @@ public class BinarySearchLayoutTest extends LayoutTestBase {
 
     @Override
     protected Layout createLayout() {
-        return new BinarySearchLayout(keyboard);
+        return new BinarySearchLayout(keyboard, new SingleThreadSuggestions(keyboard, new Dictionary() {
+            @Override
+            public List<String> getSuggestionsStartingWith(String match) {
+                return Collections.emptyList();
+            }
+
+            @Override
+            public void updateWordUsage(String string) {
+
+            }
+        }));
     }
 
     @Test
