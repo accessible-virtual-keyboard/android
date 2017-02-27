62383613a44c49ec89e6d095b864f777814fbc23
Kristian Honningsvag <kristian@digitalunion.net>
2017-02-27 13:12:10 +0100

    Binary dictionary test for two frequencies

--- app/src/main/java/no/ntnu/stud/avikeyb/backend/dictionary/BinaryDictionary.java
+++ app/src/main/java/no/ntnu/stud/avikeyb/backend/dictionary/BinaryDictionary.java
@@ -122,14 +122,12 @@ public class BinaryDictionary implements Dictionary, InMemoryDictionary {
         Collections.sort(matchingDictionaryEntries, new Comparator<DictionaryEntry>() {
             @Override
             public int compare(DictionaryEntry o1, DictionaryEntry o2) {
-                System.out.println(o1.toString());
                 return o2.getStandardFrequency() - o1.getStandardFrequency();
             }
         });
         Collections.sort(matchingDictionaryEntries, new Comparator<DictionaryEntry>() {
             @Override
             public int compare(DictionaryEntry o1, DictionaryEntry o2) {
-                System.out.println(o1.toString());
                 return o2.getUserFrequency() - o1.getUserFrequency();
             }
         });
