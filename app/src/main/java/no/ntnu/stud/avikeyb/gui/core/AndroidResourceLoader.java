2fc7c193103e0e8fa5b9ef1adadb4982b9fa0623
Kristian Honningsvag <kristian@digitalunion.net>
2017-02-27 10:44:13 +0100

    ResourceLoader now handles two types of frequencies.

--- app/src/main/java/no/ntnu/stud/avikeyb/gui/core/AndroidResourceLoader.java
+++ app/src/main/java/no/ntnu/stud/avikeyb/gui/core/AndroidResourceLoader.java
@@ -1,25 +1,38 @@
 package no.ntnu.stud.avikeyb.gui.core;
 
 import android.content.Context;
+import android.nfc.FormatException;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import no.ntnu.stud.avikeyb.backend.dictionary.DictionaryEntry;
 import no.ntnu.stud.avikeyb.backend.dictionary.ResourceLoader;
 
 /**
- * Android specific resource loader
+ * Android specific resource loader.
+ *
+ * @author ?
+ * @author Kristian Honningsvag.
  */
 public class AndroidResourceLoader {
 
     /**
-     * Loads a dictionary from the provided file resource id
+     * Loads a dictionary from the provided file resource id.
      *
      * @param context    a context object
      * @param resourceId the resource id of the file to load
      * @return a list of dictionary entries
      */
     public static List<DictionaryEntry> loadDictionaryFromResource(Context context, int resourceId) {
-        return ResourceLoader.loadDictionaryFromStream(context.getResources().openRawResource(resourceId));
+        List<DictionaryEntry> dictionary = new ArrayList<>();
+        try {
+            dictionary = ResourceLoader.loadDictionaryFromStream(context.getResources().openRawResource(resourceId));
+        } catch (Exception e) {
+            System.err.println("Failed loading dictionary from resourceID " + resourceId);
+            e.printStackTrace();
+        }
+        return dictionary;
     }
 }
