5b1a8512bf848d0fc7ce59066ef8a6b5d53ea136
Pitmairen <progrper@gmail.com>
2017-02-27 13:28:43 +0100

    Sort dictionary entries for the binary dictionary

--- app/src/test/java/no/ntnu/stud/avikeyb/backend/dictionary/StandardTestsBinaryDictionary.java
+++ app/src/test/java/no/ntnu/stud/avikeyb/backend/dictionary/StandardTestsBinaryDictionary.java
@@ -1,5 +1,6 @@
 package no.ntnu.stud.avikeyb.backend.dictionary;
 
+import java.util.Collections;
 import java.util.List;
 
 import no.ntnu.stud.avikeyb.backend.Dictionary;
@@ -11,6 +12,8 @@ import no.ntnu.stud.avikeyb.backend.Dictionary;
 public class StandardTestsBinaryDictionary extends DictionaryTester {
     @Override
     protected Dictionary createDictionary(List<DictionaryEntry> entries) {
+        // The binary dictionary expects the entries to be sorted alphabetically
+        Collections.sort(entries, (e1,  e2) -> e1.getWord().compareTo(e2.getWord()));
         return new BinaryDictionary(entries);
     }
 }
